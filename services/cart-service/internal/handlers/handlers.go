7fecc3694454ec636158a01ccfa65b969a8a8241
Manash <rinpratim0635@gmail.com>
2026-01-11 16:05:25 +0530

    implemented psot /api/cart/items

--- services/cart-service/internal/handlers/handlers.go
+++ services/cart-service/internal/handlers/handlers.go
@@ -1,13 +1,15 @@
 package handlers
 
 import (
-	"database.sql"
+	"database/sql"
 	"net/http"
 	"errors"
+	"encoding/json"
 
 	"github.com/google/uuid"
 	"github.com/herodragmon/scalable-ecommerce/services/cart-service/internal/config"
 	"github.com/herodragmon/scalable-ecommerce/services/cart-service/internal/response"
+	"github.com/herodragmon/scalable-ecommerce/services/cart-service/internal/database"
 )
 
 func RegisterRoutes(mux *http.ServeMux, cfg *config.Config) {
@@ -17,6 +19,10 @@ func RegisterRoutes(mux *http.ServeMux, cfg *config.Config) {
 
 	mux.HandleFunc("GET /api/cart", func(w http.ResponseWriter, r *http.Request) {
 		handlerCartGet(cfg, w, r)
+	})
+
+	mux.HandleFunc("POST /api/cart/items", func(w http.ResponseWriter, r *http.Request) {
+		handlerCartAddItem(cfg, w, r)
 	})
 }
 
@@ -81,5 +87,98 @@ func handlerCartGet(cfg *config.Config, w http.ResponseWriter, r *http.Request) 
 		ID:         cart.ID,
 		Items:      itemResponses,
 		TotalCents: totalCents,
+	})
+}
+
+func handlerCartAddItem(cfg *config.Config, w http.ResponseWriter, r *http.Request) {
+	type addItemRequest struct {
+    ProductID uuid.UUID `json:"product_id"`
+    Quantity  int32     `json:"quantity"`
+	}
+	type CartItemResponse struct {
+    ID         uuid.UUID `json:"id"`
+    ProductID  uuid.UUID `json:"product_id"`
+    Quantity   int32     `json:"quantity"`
+    PriceCents int32     `json:"price_cents"`
+	}
+
+	userIDStr := r.Header.Get("X-User-ID")
+	if userIDStr == "" {
+		response.RespondWithError(w, http.StatusUnauthorized, "missing user ID", nil)
+		return
+	}
+	userID, err := uuid.Parse(userIDStr)
+	if err != nil {
+		response.RespondWithError(w, http.StatusBadRequest, "invalid user ID", err)
+		return
+	}
+
+	var item addItemRequest
+	err = json.NewDecoder(r.Body).Decode(&item)
+	if err != nil {
+		response.RespondWithError(w, http.StatusBadRequest, "couldn't decode json", err)
+		return
+	}
+
+	if item.ProductID == uuid.Nil || item.Quantity <= 0 {
+		response.RespondWithError(w, http.StatusBadRequest, "invalid product_id or quantity", err)
+		return
+	}
+
+	product, exists, err:= cfg.ProductClient.GetProduct(r.Context(), item.ProductID)
+	if err != nil {
+		response.RespondWithError(w, http.StatusInternalServerError, "error checking product", err)
+		return
+	}
+	if !exists {
+    response.RespondWithError(w, http.StatusNotFound, "product not found", nil)
+    return
+	}
+
+	cart, err := cfg.DB.GetCartByUserID(r.Context(), userID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			cart, err = cfg.DB.CreateCart(r.Context(), userID)
+			if err != nil {
+				response.RespondWithError(w, http.StatusInternalServerError, "couldn't create cart", err)
+				return
+      }
+    } else {
+			response.RespondWithError(w, http.StatusInternalServerError, "couldn't get cart", err)
+			return
+    }
+	}
+
+	existingItem, err := cfg.DB.GetCartItemByProductID(r.Context(), database.GetCartItemByProductIDParams{
+    CartID:    cart.ID,
+    ProductID: item.ProductID,
+	})
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+    response.RespondWithError(w, http.StatusInternalServerError, "couldn't check cart", err)
+    return
+	}
+	var cartItem database.CartItem
+	if err == nil {
+		cartItem, err = cfg.DB.UpdateCartItemQuantity(r.Context(), database.UpdateCartItemQuantityParams{
+			ID:       existingItem.ID,
+      Quantity: existingItem.Quantity + item.Quantity,
+    })
+	} else {
+			cartItem, err = cfg.DB.AddCartItem(r.Context(), database.AddCartItemParams{
+        CartID:     cart.ID,
+        ProductID:  item.ProductID,
+        Quantity:   item.Quantity,
+        PriceCents: product.PriceCents,
+    	})
+		}
+	if err != nil {
+    response.RespondWithError(w, http.StatusInternalServerError, "couldn't add item to cart", err)
+    return
+	}
+	response.RespondWithJSON(w, http.StatusCreated, CartItemResponse{
+    ID:         cartItem.ID,
+    ProductID:  cartItem.ProductID,
+    Quantity:   cartItem.Quantity,
+    PriceCents: cartItem.PriceCents,
 	})
 }
